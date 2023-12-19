e25afbe6d53957fabbec314ae3f54cdd31a5ae94
comghost <overpassion@gmail.com>
2023-12-19 16:51:46 +0900

    Update egovframe-ide.rb
    Modify unnecessary code after completing code signing process

--- Casks/egovframe-ide.rb
+++ Casks/egovframe-ide.rb
@@ -4,13 +4,13 @@ cask "egovframe-ide" do
     app_name = ""
   
     on_arm do
-      sha256 "7a3f276cb8b4accb01eca47f9a65f4abaabd5f7eb9d2c0862909d9e26602afde"
+      sha256 "7e7ece1b05a6e4ccfca522a44f4a4e0230a164f8542df0e78c0181efc039590e"
   
       url "https://maven.egovframe.go.kr/publist/HDD1/public/eGovFrameDev-#{version}-Mac-AArch64.dmg"
       app_name = "eGovFrameDev-#{version}-Mac-AArch64.app"
     end
     on_intel do
-      sha256 "53399206e310112396dd294bb24069e59e32fe9694e1bd6f0e0acc73fef95ba7"
+      sha256 "b3c652165914f7ccc05eae9e4730954a0a2cf0d361331a14f2c291fa51510652"
   
       url "https://maven.egovframe.go.kr/publist/HDD1/public/eGovFrameDev-#{version}-Mac-x86_64.dmg"
       app_name = "eGovFrameDev-#{version}-Mac-x86_64.app"
@@ -30,9 +30,6 @@ cask "egovframe-ide" do
   
     postflight do
       set_ownership "/Applications/#{app_name}"
-      system_command "/usr/bin/xattr",
-                     args: ["-cr", "/Applications/#{app_name}"],
-                     sudo: true
     end
   
     zap trash: [
